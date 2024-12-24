8837dfa78684ac20e0dbf0bafd041a90b0be10c4
Calvaria <1764203060@qq.com>
2024-12-24 21:57:56 +0800

    add ipc file

---
+++ project/code/ipc/ipc.c
@@ -0,0 +1,8 @@
+#include "ipc/cy_ipc_pipe.h"
+#include "ipc/cy_ipc_config.h"
+#include "ipc.h"
+
+void init_ipc()
+{
+    
+}
---
+++ project/code/ipc/ipc.h
@@ -0,0 +1,7 @@
+#ifndef IPC_H_
+#define IPC_H_
+
+#include "zf_common_typedef.h"
+
+
+#endif /* IPC_H_ */
