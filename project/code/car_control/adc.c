20ff6d9633f6368f8814095b028b0b5dc669f567
Calvaria <17198186+Calvariaa@users.noreply.github.com>
2025-04-15 22:30:08 +0800

    a

--- project/code/car_control/adc.c
+++ project/code/car_control/adc.c
@@ -18,11 +18,33 @@
 #include "zf_driver_adc.h"
 #include "zf_driver_gpio.h"
 
-
-adc_channel_enum adc_tube[1] = {ADC2_CH06_P18_6};
+adc_channel_enum adc_tube[20] = {
+    // ×îÓÒ±ß¿ªÊ¼Íù×ó
+    ADC0_CH06_P06_6,
+    ADC0_CH07_P06_7,
+    ADC0_CH16_P07_0,
+    ADC0_CH23_P07_7,
+    ADC1_CH04_P12_0,
+    ADC0_CH24_P08_1,
+    ADC0_CH25_P08_2,
+    //
+    ADC0_CH26_P08_3,
+    ADC1_CH08_P12_4,
+    ADC1_CH09_P12_5,
+    ADC1_CH12_P13_0,
+    ADC1_CH19_P13_7,
+    ADC1_CH25_P14_5,
+    //
+    ADC2_CH02_P18_2,
+    ADC2_CH00_P18_0,
+    ADC2_CH03_P18_3,
+    ADC2_CH05_P18_5,
+    ADC2_CH01_P18_1,
+    ADC2_CH07_P18_7,
+    ADC2_CH06_P18_6
+};
 
 uint16_t adc_tube_read_raw[20] = {0};
-
 
 void tube_adc_init()
 {
